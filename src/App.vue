360dfa0717ebcd069922e0c17d0ec9fc618251df
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2023-08-25 16:19:53 -0300

    create

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="calculator">
+    <input v-model="display" class="display" disabled />
+    <div class="buttons">
+      <button v-for="button in buttons" :key="button" @click="handleClick(button)">
+        {{ button }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      display: "",
+      buttons: ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"],
+    };
+  },
+  methods: {
+    handleClick(button) {
+      if (button === "=") {
+        try {
+          this.display = eval(this.display).toString();
+        } catch (error) {
+          this.display = "Error";
+        }
+      } else {
+        this.display += button;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.calculator {
+  width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.display {
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 18px;
+  text-align: right;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+
+button {
+  padding: 10px;
+  font-size: 16px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #ccc;
+}
+</style>
+
